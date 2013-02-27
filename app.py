894a517b035bf380c333412505daa156d405f20d
David Johnson <djohnson.m@gmail.com>
2013-02-27 10:37:26 -0600

    pos fix

--- app.py
+++ app.py
@@ -68,8 +68,7 @@ def validate():
 @app.route('/checkout/')
 def checkout():
     checkout = "https://hormonalnutrition.herokuapp.com/checkout/"
-    print (request.headers.get('X-Forwarded-Proto', 'http') == 'https' and request.url.startswith(checkout))
-    if request.headers.get('X-Forwarded-Proto', 'http') == 'https' and request.url.startswith(checkout):
+    if request.headers.get('X-Forwarded-Proto', 'http') == 'https':
         return set_hsts_header(make_response(render_template('checkout.html', key=stripe_keys['publishable_key'])))
     return redirect(checkout, code=302)
 
